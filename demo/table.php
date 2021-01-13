94b01343cf06c18ae0a1dd2e4f5a9db60117906c
Amashige Seiji <tenjuu99@gmail.com>
2021-01-14 03:26:41 +0900

    update demo

--- demo/table.php
+++ demo/table.php
@@ -15,10 +15,21 @@ use tenjuu99\ORM\TableModule;
 $meta = new class() extends AbstractAppMeta {};
 $meta->name = 'MyVendor\Demo';
 
-$module = new ResourceModule('MyVendor\Demo');
+$module = new ResourceModule($meta->name);
 $module->install(new OrmModule('sqlite'));
 $module->install(new PdoModule('sqlite:demo/table-demo.db'));
 $module->override(new TableModule($meta));
+$injector = new Injector($module, __DIR__ . '/tmp');
+/** @var PDO */
+$pdo = $injector->getInstance(PDO::class);
+$pdo->query('CREATE TABLE IF NOT EXISTS sample(id integer, name varchar(255))')->execute();
+$pdo->query('INSERT INTO sample(id, name) VALUES (1, "test 1")')->execute();
+$pdo->query('INSERT INTO sample(id, name) VALUES (2, "test 2")')->execute();
 /** @var ResourceInterface */
-$resource = (new Injector($module, __DIR__ . '/tmp'))->getInstance(ResourceInterface::class);
-var_dump($resource->get('table://self/sample')->body);
+$resource = $injector->getInstance(ResourceInterface::class);
+//var_dump($resource->get('table://self/sample')->body['sample']);
+
+foreach ($resource->get('table://self/sample')->body['sample'] as $row) {
+    var_dump($row);
+}
+unlink('demo/table-demo.db');
